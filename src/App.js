2340d6e28ccc4a6eda80817e34031860c0991280
SorinC6 <kish.sorin@yahoo.com>
2022-09-09 14:00:42 +0300

    add login component and selectors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
-import Public from './components/Home';
+import Home from './components/Home';
+import Layout from './components/Layout'
+import Login from './features/auth/Login'
+import Welcome from './features/auth/Welcome'
+import RequireAuth from './features/auth/RequireAuth'
+// import UsersList from './features/users/UsersList'
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Public />} />
+        <Route path="/" element={Layout}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* protected routes */}
+          <Route element={<RequireAuth />} >
+            <Route path="/welcome" element={<Welcome />} />
+          </Route>
+
+        </Route>
       </Routes>
     </div>
   );
