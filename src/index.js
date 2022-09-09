50e88d90b662ff02faf7780eb6e6fae8d453bde1
SorinC6 <kish.sorin@yahoo.com>
2022-09-09 12:35:18 +0300

    add redux toolkit setup and basic omponents

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
